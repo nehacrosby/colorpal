67351b3cb5b7d95d74c74e124d9fbf985b01fc5b
Neha Crosby <nehacrosby@nehacrosby-macbookair.local>
2012-06-14 01:00:02 -0700

    Insert video tutorial js

---
+++ video_tutorials.js
@@ -0,0 +1,22 @@
+// This library contains the handling of playing a video
+// tutorial.
+
+// TODO(Neha): Figure out why the video isn't always clickable.
+Video = {
+  init: function() {    
+    // Add all the click handlers.
+    $(".video-watch").click(jQuery.proxy(this.onVideoClick, this)); 
+  },
+  
+  onVideoClick: function(event) {
+    console.log($(event.currentTarget).find('img').attr('src'));
+    this.playTutorial($(event.currentTarget).find('img').attr('src'));  
+  },
+  
+  playTutorial: function(filename) {
+    $("#listScreen").hide();
+    $("#videoScreen").show();
+    console.log("I've watched the video");  
+    UserPrefs.saveCompletedImage(ImageLibrary[App.imageIndex].filename); 
+  }
+}
